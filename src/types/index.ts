92dbced2196460063005c619186aae1d60979b8e
yukee520 <yukee92tmc@hotmail.com>
2026-09-15 18:26:51 +0800

    Create index.ts [skip ci]

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export * from './common';
+export * from './painPoint';
+export * from './idea';
+export * from './template';
+export * from './marketing';
+export * from './settings';
+export * from './n8n';
