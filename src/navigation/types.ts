6676cba98c5816b71b0e4d12646dc886510d8cbd
yukee520 <yukee92tmc@hotmail.com>
2026-09-15 23:38:57 +0800

    Update types.ts [skip ci]

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,4 +1,36 @@
-export type RootStackParamList = {
-  Home: undefined;
-  Details: undefined;
+import type { NavigatorScreenParams } from '@react-navigation/native';
+import type { MarketingChannel } from '@/types/marketing';
+import type { ID } from '@/types/common';
+
+export type TabParamList = {
+  Dashboard: undefined;
+  PainPoints: undefined;
+  Ideas: undefined;
+  Templates: undefined;
+  Marketing: undefined;
 };
+
+export type RootStackParamList = {
+  Tabs: NavigatorScreenParams<TabParamList>;
+  PainPointForm: { id?: ID } | undefined;
+  PainPointDetail: { id: ID };
+  IdeaDetail: { id: ID };
+  BuildStatus: { templateId: ID };
+  TemplateDetail: { id: ID };
+  TemplateTest: { id: ID };
+  Guide: { id: ID };
+  Pricing: { id: ID };
+  Listing: { id: ID };
+  MarketingList: { templateId?: ID } | undefined;
+  MarketingDetail: { templateId: ID; channel: MarketingChannel };
+  Published: undefined;
+  Settings: undefined;
+  About: undefined;
+};
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace ReactNavigation {
+    interface RootParamList extends RootStackParamList {}
+  }
+}
